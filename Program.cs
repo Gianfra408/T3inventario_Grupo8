32a5e0e066b8eb852bd8a817cc6c5ce89b4a329e
Alu-0 <hellscriptures@gmail.com>
2026-06-16 17:39:11 -0500

    Implementa registro de productos

--- Program.cs
+++ Program.cs
@@ -20,17 +20,91 @@ class Program
             Console.WriteLine("\n===== MENU =====");
             Console.WriteLine("1. Registrar producto");
             Console.WriteLine("2. Mostrar productos");
-            Console.WriteLine("3. Buscar por código");
+            Console.WriteLine("3. Buscar por c贸digo");
             Console.WriteLine("4. Modificar producto");
-            Console.WriteLine("5. Insertar en posición");
+            Console.WriteLine("5. Insertar en posici贸n");
             Console.WriteLine("6. Eliminar producto");
             Console.WriteLine("7. Ordenar por burbuja");
             Console.WriteLine("8. Resumen");
             Console.WriteLine("9. Salir");
 
-            Console.Write("Opción: ");
+            Console.Write("Opci贸n: ");
             opcion = Convert.ToInt32(Console.ReadLine());
 
+            switch (opcion)
+            {
+                case 1:
+                    Registrar();
+                    break;
+                case 2:
+                    Mostrar();
+                    break;
+                case 3:
+                    Buscar();
+                    break;
+                case 4:
+                    Modificar();
+                    break;
+                case 5:
+                    Insertar();
+                    break;
+                case 6:
+                    Eliminar();
+                    break;
+                case 7:
+                    OrdenarBurbuja();
+                    break;
+                case 8:
+                    Resumen();
+                    break;
+                case 9:
+                    Console.WriteLine("Fin del programa");
+                    break;
+                default:
+                    Console.WriteLine("Opci贸n incorrecta");
+                    break;
+            }
+
         } while (opcion != 9);
+    }
+
+    static void Registrar()
+    {
+
+    }
+
+    static void Mostrar()
+    {
+
+    }
+
+    static void Buscar()
+    {
+
+    }
+
+    static void Modificar()
+    {
+
+    }
+
+    static void Insertar()
+    {
+
+    }
+
+    static void Eliminar()
+    {
+
+    }
+
+    static void OrdenarBurbuja()
+    {
+
+    }
+
+    static void Resumen()
+    {
+
     }
 }
